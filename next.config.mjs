9740dda0eb8652978a3630fb5b16b3b914790d6d
tiago-bacelar <pereira.migueltiago@gmail.com>
2024-09-13 15:20:14 +0100

    fix redirect for static pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,6 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: '/minecraft-qr-code',
-                destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-                permanent: false,
-                basePath: false
-            },
-        ]
-    },
 };
 
 
---
+++ src/app/minecraft-qr-code/page.tsx
@@ -0,0 +1,4 @@
+import { redirect } from 'next/navigation';
+export default async function Home() {
+    redirect('https://www.youtube.com/watch?v=xvFZjo5PgG0');
+}
